247b976308aa95cf73adc4537c18f5bf8ba00495
fwcd <fwcdmail@gmail.com>
2022-08-29 15:50:38 +0200

    Set up a GUI

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-use anyhow::Result;
-use tracing::info;
+use druid::{AppLauncher, WindowDesc, widget::Label};
 
-fn bootstrap_tracing() -> Result<()> {
+fn bootstrap_tracing() {
     let subscriber = tracing_subscriber::FmtSubscriber::new();
-    tracing::subscriber::set_global_default(subscriber)?;
-    Ok(())
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Could not set up tracing subscriber");
+}
+
+fn run_gui() {
+    let state = ();
+    let window = WindowDesc::new(Label::new("Hello world!"))
+        .title("Robo")
+        .window_size((640., 480.));
+    AppLauncher::with_window(window)
+        .launch(state)
+        .expect("Could not launch GUI");
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    bootstrap_tracing()?;
-
-    info!("Hello, world!");
-
-    Ok(())
+async fn main() {
+    bootstrap_tracing();
+    run_gui();
 }
