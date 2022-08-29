601b544d2260fd8a49d874c6207b43910ebc036a
fwcd <fwcdmail@gmail.com>
2022-08-29 15:41:59 +0200

    Set up a basic Tokio app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tracing::info;
+
+fn bootstrap_tracing() -> Result<()> {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber)?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    bootstrap_tracing()?;
+
+    info!("Hello, world!");
+
+    Ok(())
 }
