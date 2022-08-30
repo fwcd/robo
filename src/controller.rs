1e735a86a709595ad42a4e0281fa077b0f117271
fwcd <fwcdmail@gmail.com>
2022-08-30 14:39:05 +0200

    Add mouse handling

--- src/controller.rs
+++ src/controller.rs
@@ -1,9 +1,17 @@
-use enigo::{Enigo, KeyboardControllable};
+use enigo::{Enigo, KeyboardControllable, MouseControllable};
 
-use crate::protocol::Action;
+use crate::protocol::{Action, MouseButton};
 
 pub struct Controller {
     enigo: Enigo,
+}
+
+fn to_enigo_button(mouse_button: MouseButton) -> enigo::MouseButton {
+    match mouse_button {
+        MouseButton::Left => enigo::MouseButton::Left,
+        MouseButton::Middle => enigo::MouseButton::Middle,
+        MouseButton::Right => enigo::MouseButton::Right,
+    }
 }
 
 impl Controller {
@@ -13,9 +21,12 @@ impl Controller {
 
     pub fn perform(&mut self, action: Action) {
         match action {
-            Action::KeySequence { text } => {
-                self.enigo.key_sequence(&text);
-            },
+            Action::KeySequence { text } => self.enigo.key_sequence(&text),
+            Action::MouseMoveTo { point } => self.enigo.mouse_move_to(point.x, point.y),
+            Action::MouseMoveBy { delta } => self.enigo.mouse_move_relative(delta.x, delta.y),
+            Action::MouseDown { button } => self.enigo.mouse_down(to_enigo_button(button)),
+            Action::MouseUp { button } => self.enigo.mouse_up(to_enigo_button(button)),
+            Action::MouseClick { button } => self.enigo.mouse_click(to_enigo_button(button)),
         }
     }
 }
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,7 +1,40 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Vec2<T> {
+    pub x: T,
+    pub y: T,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum MouseButton {
+    Left,
+    Middle,
+    Right,
+}
+
+impl Default for MouseButton {
+    fn default() -> Self { Self::Left }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 pub enum Action {
+    // Keyboard
     KeySequence { text: String },
+    // Mouse
+    MouseMoveTo { point: Vec2<i32> },
+    MouseMoveBy { delta: Vec2<i32> },
+    MouseDown {
+        #[serde(default)]
+        button: MouseButton
+    },
+    MouseUp {
+        #[serde(default)]
+        button: MouseButton
+    },
+    MouseClick {
+        #[serde(default)]
+        button: MouseButton
+    },
 }
